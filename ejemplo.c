02bf81f8c715a13c374f6755f522390733a15738
FloresPatwil <pfloresri@unsa.edu.pe>
2022-06-15 23:02:24 -0400

    Intento por unir los cuadros

--- ejemplo.c
+++ ejemplo.c
@@ -2,8 +2,14 @@
 #include "figures.h"
 
 void display(){
-  char** blackKnight = reverse(knight);
+  /*char** blackKnight = reverse(knight);
   interpreter(blackKnight);
   char** Square = repeatH(whiteSquare, 8);
   interpreter(Square);
+  */
+  char** Black = reverse(repeatH(whiteSquare,0));
+  char** White = repeatH(whiteSquare,0);
+
+  char** Union = join(Black, White);
+  interpreter(Union);
 }
