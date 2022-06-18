ccd5f7bd9d1cb5c7ee1da6c8eebc22eddeacdc9d
FloresPatwil <pfloresri@unsa.edu.pe>
2022-06-18 18:14:25 -0400

    Modificando archivo chess.c

--- chess.c
+++ chess.c
@@ -1,42 +1,51 @@
 #include "chess.h"
 #include "figures.h"
 
-char** flipV(char** y){
+char** flipV(char** y){  //espejo vertical de la imagen original
+    char *p;
+    p = y;
+    for(int i=0;i<sizeof(y);i++){
+        while(*p != '\0'){
+            y[i] = strev(p);
+            p++;
+        }
+    }
+    return y;
+}
+
+char** flipH(char** y){  //espejo horizontal de la imagen original
+    for(int i=sizeof(y); i>=0;i--)
+        return y;
+}
+
+char** rotateL(char** y){  //Nueva imagen de la rotacion a la izquierda del original
 
 }
 
-char** flipH(char** y){
+char** rotateR(char** y){  //Nueva imagen de la rotacion a la derecha del original
 
 }
 
-char** rotateL(char** y){
+char** reverse(char** y){  //imagen contraria a la original
 
 }
 
-char** rotateR(char** y){
+char** superImpose(char** y, char** x){  //imagen resultado de imagen superpuesta a otra
 
 }
 
-char** reverse(char** y){
+char** join(char** y, char** x){  //imagen resultado de una imagen al lado de otra
 
 }
 
-char** superImpose(char** y, char** x){
+char** up(char** y, char** x){  //imegen resultado de una imagen sobre otra
 
 }
 
-char** join(char** y, char** x){
+char** repeatH(char** y, int x){  //imagen resultado de repetir horizontalmente
 
 }
 
-char** up(char** y, char** x){
-
-}
-
-char** repeatH(char** y, int x){
-
-}
-
-char** repeatV(char** y, int x){
+char** repeatV(char** y, int x){  //imagen resultado de repetir verticalmente
 
 }
