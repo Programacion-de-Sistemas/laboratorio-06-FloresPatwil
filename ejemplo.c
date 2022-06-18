a14dd810199b4e66d4738a4f11ee671b7dbb9e6b
FloresPatwil <pfloresri@unsa.edu.pe>
2022-06-17 21:52:36 -0400

    Provando Flip para ejercicio 3

--- ejemplo.c
+++ ejemplo.c
@@ -1,23 +1,28 @@
 #include "chess.h"
 #include "figures.h"
 void display(){
-  /*char** blackKnight = reverse(knight);
+  /* char** blackKnight = reverse(knight);
   interpreter(blackKnight);
-  char** Square = repeatH(whiteSquare, 8);
-  interpreter(Square);*/
-  //---------------------------------------
-  /*EJERCICIO 1 cuadro blanqui negro
+  ---------------------------------------
+  EJERCICIO 1 cuadro blanqui negro
   char** Black = reverse(whiteSquare);
   char** White = whiteSquare;
   char** Union = join(White, Black);
 
   char** Suceso = repeatH(Union,4);
   interpreter(Suceso);
-  */
+  
   //EJERCICIO 2 cuadro negri blanco
   char** Black = reverse(whiteSquare);
   char** Union = join(Black, whiteSquare);
 
   char** Suceso = repeatH(Union,4);
-  interpreter(Suceso); 
+  interpreter(Suceso); */
+  //EJERCICIO 3 
+  char** Black = reverse(whiteSquare);
+  char** Union = join(Black, whiteSquare);
+
+  char** Suceso = repeatH(Union,4);
+  char** Espejo =flipV(Suceso);
+  interpreter(Espejo);
 }
