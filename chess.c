166e87e21669423fae4258b2fcef1752d469f641
FloresPatwil <pfloresri@unsa.edu.pe>
2022-06-18 18:03:51 -0400

    Completamiento de chess.c

--- chess.c
+++ chess.c
@@ -1,1 +1,42 @@
-#include <stdio.h>
+#include "chess.h"
+#include "figures.h"
+
+char** flipV(char** y){
+
+}
+
+char** flipH(char** y){
+
+}
+
+char** rotateL(char** y){
+
+}
+
+char** rotateR(char** y){
+
+}
+
+char** reverse(char** y){
+
+}
+
+char** superImpose(char** y, char** x){
+
+}
+
+char** join(char** y, char** x){
+
+}
+
+char** up(char** y, char** x){
+
+}
+
+char** repeatH(char** y, int x){
+
+}
+
+char** repeatV(char** y, int x){
+
+}
