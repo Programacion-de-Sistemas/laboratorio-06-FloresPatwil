d1a3e90909c27d9862068ec12483684b11dfc91b
FloresPatwil <pfloresri@unsa.edu.pe>
2022-06-18 19:45:33 -0400

    Se modifico y comento las funciones

--- chess.c
+++ chess.c
@@ -20,18 +20,18 @@ char** flipH(char** y){  //espejo horizontal de la imagen original
 }
 
 char** rotateL(char** y){  //Nueva imagen de la rotacion a la izquierda del original
-    char temp = y[0];
-    for(int i=0; i<(sizeof(y)-1);i++)
-        y[i] = y[i+1];
+    char temp = y[0];  //agarramos el primer array
+    for(int i=0; i<(sizeof(y)-1);i++)  //rrecorremos el array
+        y[i] = y[i+1];  //rotamos
     return y;
 }
 
 char** rotateR(char** y){  //Nueva imagen de la rotacion a la derecha del original
-    int tamaño = sizeof(y);
+    int tamaño = sizeof(y);  //tamano del array
     int n = tamaño;
-    char temp = y[tamaño-1];
-    for(int j=0; j<n;i++){
-        for (int i=(tamaño-2);i>=0;i--)
+    char temp = y[tamaño-1];  //tomamos el ultimo elemento   
+    for(int j=0; j<n;i++){  //recorremos el array
+        for (int i=(tamaño-2);i>=0;i--)  //voltemos a la derecha uno por uno
             y[i+1] = y[i];
         y[0]=temp;
     }
@@ -39,12 +39,12 @@ char** rotateR(char** y){  //Nueva imagen de la rotacion a la derecha del origin
 }
 
 char** reverse(char** y){  //imagen contraria a la original
-    char *p;
+    char *p;  //creamos puntero
     p=y;
-    int tamaño = sizeof(y);
-    for(int i=0;j<tamaño;i++){
-        while(*p != '\0'){
-            if(*p =='_' || *p =='.'){
+    int tamaño = sizeof(y);  //tamano del array
+    for(int i=0;j<tamaño;i++){  //recorre el array
+        while(*p != '\0'){  //todos los que no cean 0
+            if(*p =='_' || *p =='.'){  //invierte el color definido
                 *p='@';
             }else if (*p =='='){
                 *p='#';
@@ -65,17 +65,17 @@ char** superImpose(char** y, char** x){  //imagen resultado de imagen superpuest
 }
 
 char** join(char** y, char** x){  //imagen resultado de una imagen al lado de otra
-    int tamañoY=sizeof(y);
-    int tamañoX=sizeof(x);
+    int tamañoY=sizeof(y);  //tam 1 cadena
+    int tamañoX=sizeof(x);  //tam 2 cadena
     int cadenas=0;
 
     for(int i=0;i<tamañoY;i++){
-        int cadenas += strlen(y[i]);
+        int cadenas =+ strlen(y[i]);  //extraemos elem de la cadena
     }
-    int tamañoZ = cadenas+tamañoY*strlen(x);
+    int tamañoZ = cadenas+tamañoY*strlen(x); //juntamos los tamnos de las cadenas
     char** z=malloc((tamañoZ+1)*sizeof(char));
-    z[tamañoZ] = '\0';
-
+    z[tamañoZ] = '\0';  //almacenmos en la cadena
+    //recorremos segunda cadena
     int temp = 0;
     for(int i=0;i<tamañoY;i++){
         if(i != 0){
@@ -83,20 +83,21 @@ char** join(char** y, char** x){  //imagen resultado de una imagen al lado de ot
             temp+=strlen(x);
         }
 
-        strcpy(z+temp,y[i]);
+        strcpy(z+temp,y[i]);  //se almacena cadena Z
         temp += strlen(y[i]);
     }
-    return z;
+    return z;  //nueva cadena con ambos
 }
 
 char** up(char** y, char** x){  //imegen resultado de una imagen sobre otra
     int tamañoY=sizeof(y);
     int tamañoX=sizeof(x);
+    //se trata de unir los arrays
     char** z;
     for(int i=0;i<tamañoY;i++){
-        z[i] = y[i];
-    }for(int j=0;j<tamañoX;i++){
-        z[i++]=y[j];
+        z[i] = y[i];   
+    }for(int j=0;j<tamañoX;j++){
+        z[j++]=x[j];
     }
     for(int k=0;k<tamañoY+tamañoX;k++){
         return z[k];
@@ -104,6 +105,7 @@ char** up(char** y, char** x){  //imegen resultado de una imagen sobre otra
 }
 
 char** repeatH(char** y, int x){  //imagen resultado de repetir horizontalmente
+    //se repite n veces la funcion
     for(int i=0;i<=x;i++){
         char** z=join(char** y,char** y);
         return z;
@@ -111,6 +113,7 @@ char** repeatH(char** y, int x){  //imagen resultado de repetir horizontalmente
 }
 
 char** repeatV(char** y, int x){  //imagen resultado de repetir verticalmente
+    //se repite n veces la funcion
     for(int i=0;i<=x;i++){
         return y;    
     }
