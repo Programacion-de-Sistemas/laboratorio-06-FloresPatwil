90d4a5c31a1473cdcb58ccbe753f82fb1cf45b2a
FloresPatwil <pfloresri@unsa.edu.pe>
2022-06-17 20:35:20 -0400

    Ejercicio 1 funciona correctamente

--- ejemplo.c
+++ ejemplo.c
@@ -1,15 +1,16 @@
 #include "chess.h"
 #include "figures.h"
-
 void display(){
   /*char** blackKnight = reverse(knight);
   interpreter(blackKnight);
   char** Square = repeatH(whiteSquare, 8);
-  interpreter(Square);
-  */
+  interpreter(Square);*/
+
+  //cuadro blanqui negro
   char** Black = reverse(whiteSquare);
   char** White = whiteSquare;
-  char** Union = join(Black, White);
-  char** Suceso = repeatH(Union, 4);
-  interpreter(Union);
+  char** Union = join(White, Black);
+
+  char** Suceso = repeatH(Union,4);
+  interpreter(Suceso);
 }
